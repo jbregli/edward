fd899d419be12176dde46fbe4a7a6d14975750cc
Dustin Tran <dustinviettran@gmail.com>
2017-03-24 08:12:41 -0400

    replace DP greek args with english args

--- examples/pp_dirichlet_process.py
+++ examples/pp_dirichlet_process.py
@@ -38,7 +38,7 @@ def dirichlet_process(alpha):
   return stick_num
 
 
-dp = dirichlet_process(alpha=10.0)
+dp = dirichlet_process(10.0)
 
 # The number of sticks broken is dynamic, changing across evaluations.
 sess = tf.Session()
@@ -46,7 +46,7 @@ print(sess.run(dp))
 print(sess.run(dp))
 
 # Demo of the DirichletProcess random variable in Edward.
-base = Normal(mu=0.0, sigma=1.0)
+base = Normal(0.0, 1.0)
 
 # Highly concentrated DP.
 alpha = 1.0
